3b6fcd1b41581998d27681e2d7015575ea08a4d2
imasnabelliavenda <167957674+imasnabelliavenda@users.noreply.github.com>
2024-09-24 21:10:30 +0700

    Delete Pertemuan 2 directory

--- Pertemuan 2/tugas 1.py
+++
@@ -1,5 +0,0 @@
-total = int(input("Masukkan total belanja :"))
-
-if total > 100000:
-    print("Selamat, Anda dapat hadiah!")
-print("Terimakasih sudah berbelanja")
--- Pertemuan 2/tugas 2.py
+++
@@ -1,8 +0,0 @@
-password = '123'
-masukan = input("Masukkan password anda :")
-
-if masukan == password:
-    print("Selamat datang bos!")
-else:
-    print("Password salah, coba lagi!")
-print("Terimakasih sudah menggunakan aplikasi kami")
