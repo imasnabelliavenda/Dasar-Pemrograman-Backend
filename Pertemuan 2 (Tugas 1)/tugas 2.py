bb993bd8afea4dfa493268dd7bfa347ad03717f1
imasnabelliavenda <167957674+imasnabelliavenda@users.noreply.github.com>
2024-09-24 21:09:59 +0700

    Add files via upload

---
+++ Pertemuan 2 (Tugas 1)/tugas 2.py
@@ -0,0 +1,8 @@
+password = '123'
+masukan = input("Masukkan password anda :")
+
+if masukan == password:
+    print("Selamat datang bos!")
+else:
+    print("Password salah, coba lagi!")
+print("Terimakasih sudah menggunakan aplikasi kami")
